0f5a999d5ee6d630e0812327a213d4c2ffe54d5d
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2018-06-20 12:36:21 +0200

    Mejoras de la intefaz
    Muestra de informacion para enterdelo mejor.

--- Enfriamiento/Main.py
+++ Enfriamiento/Main.py
@@ -24,6 +24,10 @@ R_EDGES = []
 mutation_chance = 0.1 #La probabilidad de que un individuo mute
 
 def edges(paises):
+    """
+        Generamos aristas aleatorias para el grafo usando
+        la constante EDGE_CHANCE por defecto a 0.15
+    """
     edges=[]
     for i in range(paises):
         for j in range(paises):
@@ -35,7 +39,8 @@ def edges(paises):
 
 def individual(paises, colores):
     """
-        Crea una poblacion nueva de individuos
+        Crea un individuo. Las aristas solo se generan una vez
+        para los individuos de una misma poblacion.
     """
     graph = Graph(paises, colores)
     
@@ -48,13 +53,19 @@ def individual(paises, colores):
     return  graph
 
 
-def create_population(paises, colores):
-    return [individual(paises, colores) for i in range(paises)]
+def create_population(paises, colores, poblacion):
+    """
+        Crea una poblacion con los datos solicitados.
+    """
+    return [individual(paises, colores) for i in range(poblacion)]
 
 
 def get_fitness(individual):
     """
         Calcula el fitness de un individuo concreto.
+        Nuestro objetivo sera minimizar. Se penalizara sumando
+        el tamano del grafo si el color de dos paises conectados 
+        por una arista es el mismo.
     """
     fitness = 0
     for edge in individual.edges:
@@ -66,10 +77,12 @@ def get_fitness(individual):
     return fitness
 
 
-def selection_and_reproduction(population, paises):
+def selection_and_reproduction(population, paises, poblacion):
     """
+        
         Puntua todos los elementos de la poblacion (population) y se queda con los mejores
-        guardandolos dentro de 'selected'.
+        guardandolos dentro de 'selected'. El numero de seleccionados sera una tercera parte
+        del total del a poblacion.
         Despues mezcla el material genetico de los elegidos para crear nuevos individuos y
         llenar la poblacion (guardando tambien una copia de los individuos seleccionados sin
         modificar).
@@ -77,10 +90,7 @@ def selection_and_reproduction(population, paises):
         Por ultimo muta a los individuos.
   
     """
-    
-    pressure=int(paises/3)
-    if(pressure<2):
-        pressure=2
+    pressure=int(poblacion/3)
   
     fitness = [ (get_fitness(i), i) for i in population] #Calcula el fitness de cada individuo, y lo guarda en pares ordenados de la forma (5 , [1,2,1,1,4,1,8,9,4,1])
     fitness = [i[1] for i in sorted(fitness, reverse=True)] #Ordena los pares ordenados y se queda solo con el array de valores
@@ -101,11 +111,8 @@ def selection_and_reproduction(population, paises):
     return population #El array 'population' tiene ahora una nueva poblacion de individuos, que se devuelven
 
 
-def cooldown(population, paises, colores):
-
-    pressure=int(paises/3)
-    if(pressure<2):
-        pressure=2
+def cooldown(population, colores, poblacion):
+    pressure=int(poblacion/3)
     
     for i in range(len(population)-pressure):
         ind = population[i].nodes[:]
@@ -149,10 +156,8 @@ def final(population):
     return res
 
 
-def get_best(population, paises):
-    pressure=int(paises/3)
-    if(pressure<2):
-        pressure=2
+def get_best(population, paises, poblacion):
+    pressure=int(poblacion/3)
     
     fitness = [ (get_fitness(i), i) for i in population] #Calcula el fitness de cada individuo, y lo guarda en pares ordenados de la forma (5 , [1,2,1,1,4,1,8,9,4,1])
     fitness = [i[1] for i in sorted(fitness, reverse=True)] #Ordena los pares ordenados y se queda solo con el array de valores
@@ -163,16 +168,20 @@ def get_best(population, paises):
     return selected
 
 
-def algoritmo_random(paises, colores):
+def algoritmo_random(paises, colores, poblacion):
+    del R_EDGES[:]
+    
     toplevel = Toplevel()
     text = Text(toplevel)
     
-    population = create_population(paises, colores)#Inicializar una poblacion
+    population = create_population(paises, colores, poblacion)#Inicializar una poblacion
     pprint =[(get_fitness(i), i) for i in population]
     print("Poblacion Inicial:\n%s"%(pprint)) #Se muestra la poblacion inicial
     print("\n")
     
     text.insert(INSERT, "Numero de Paises = "+str(paises)+"\nNumero de Colores = "+str(colores))
+    text.insert(INSERT, "\nAristas:\n")
+    text.insert(INSERT, R_EDGES)
     text.insert(INSERT, "\nPoblacion Inicial:\n")
     for row in pprint:
         text.insert(INSERT, row)
@@ -180,8 +189,8 @@ def algoritmo_random(paises, colores):
     
     #Se evoluciona la poblacion
     for i in range(100):
-        population = selection_and_reproduction(population, paises)
-        population = cooldown(population, paises, colores)
+        population = selection_and_reproduction(population, paises, poblacion)
+        population = cooldown(population, colores, poblacion)
      
         pprint =[(get_fitness(j), j) for j in population]
         print("Generacion "+str(i+1)+":\n%s"%(pprint))
@@ -189,16 +198,19 @@ def algoritmo_random(paises, colores):
             break
     
     print("\nPoblacion Final:\n%s"%(pprint)) #Se muestra la poblacion evolucionada
-    print("\nMejores soluciones:\n%s"%([(get_fitness(j), j) for j in get_best(population, paises)]))
+    print("\nMejores soluciones:\n%s"%([(get_fitness(j), j) for j in get_best(population, paises, poblacion)]))
     print("\n\n")
     
     text.insert(INSERT, "Mejores Soluciones con "+str(i+1)+" iteraciones:\n")
     text.insert(INSERT, "")
     
-    for row in get_best(population, paises):         
+    for row in get_best(population, paises, poblacion):         
         text.insert(INSERT, row)
         fitness = get_fitness(row)
         text.insert(INSERT, " Fiteness = "+str(fitness)+"\n")
+        
+    sc = Scrollbar(toplevel)
+    sc.pack(side=RIGHT, fill=Y)
 
     text.pack()
     toplevel.mainloop()
@@ -207,19 +219,30 @@ def algoritmo_random(paises, colores):
 def algoritmo_variable():
     
     busqueda = Toplevel()
-    label1 = Label(busqueda, text="Numero de paises: ")
-    label2 = Label(busqueda, text="Numero de colores: ")
+    label1 = Label(busqueda, text="Paises: ")
+    label2 = Label(busqueda, text="Colores: ")
+    label3 = Label(busqueda, text="Individuos: ")
+    entrada3=Entry(busqueda, bd=5)
     entrada1=Entry(busqueda, bd=5)
     entrada2=Entry(busqueda, bd=5)
 
     def buttCallBack():
         paises=entrada1.get()
         colores=entrada2.get()
+        individuos=entrada3.get()
         if paises=='':
             paises=10
+        elif int(paises)<2:
+            paises=2
         if colores=='':
             colores=3
-        algoritmo_random(int(paises), int(colores))
+        elif int(colores)<2:
+            colores=2
+        if individuos=='':
+            individuos=10
+        elif int(individuos)<6:
+            individuos=6  
+        algoritmo_random(int(paises), int(colores), int(individuos))
         
         
     butt=Tkinter.Button(busqueda, text="Ejecutar", command = buttCallBack)
@@ -229,6 +252,9 @@ def algoritmo_variable():
     
     label2.pack(side=LEFT)
     entrada2.pack(side=LEFT)
+    
+    label3.pack(side=LEFT)
+    entrada3.pack(side=LEFT)
     
     butt.pack(side=LEFT)
     
@@ -240,13 +266,20 @@ def principal():
      
     menubar = Menu(top)
     
+    text = Text(top)
+    
     dm = Menu(menubar, tearoff=0)
-    dm.add_command(label="Estandar", command=lambda: algoritmo_random(10, 3)) #, command=
+    dm.add_command(label="Estandar", command=lambda: algoritmo_random(10, 3, 10)) #, command=
     dm.add_command(label="Variable", command=algoritmo_variable)
     dm.add_command(label="Salir", command=top.destroy)
     menubar.add_cascade(label="Algoritmo", menu=dm)
     
+    text.insert(INSERT, "Estandar: Paises=10, Colores=3, Poblacion=10\n")
+    text.insert(INSERT, "Variable: Paises>=2, Colores>=2, Poblacion>=6\nCampos vacios tendran valores estandar")
+    
+    text.pack()
     top.config(menu=menubar)
+    
     
     top.mainloop()
     
